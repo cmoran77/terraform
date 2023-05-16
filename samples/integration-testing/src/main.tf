583efb918e844b5a578ea5c7c9ddfbbc7edbfa8a
cmoran77 <cmoran.mena@gmail.com>
2023-05-16 11:06:52 -0600

    test

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -4,3 +4,21 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-hello-tf-${random_uuid.uuid.result}"
   location = var.location
 }
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create a subnet
+resource "azurerm_subnet" "test" {
+   name                 = "subnetTF"
+   resource_group_name  = azurerm_resource_group.rg.name
+   virtual_network_name = azurerm_virtual_network.vnet.name
+   address_prefixes     = ["10.0.1.0/24"]
+ }
+
+ 
